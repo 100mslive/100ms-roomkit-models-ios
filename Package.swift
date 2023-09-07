c000309574f8a8d02bba225c3f420dd834b41ca7
pawan-100ms <pawan@100ms.live>
2023-09-07 11:54:00 +0400

    Making "HMSSDK", "HMSAnalyticsSDK", "WebRTC" visible to other packages

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HMSRoomModels",
-            targets: ["HMSRoomModels"]),
+            targets: ["HMSRoomModels", "HMSSDK", "HMSAnalyticsSDK", "WebRTC"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
