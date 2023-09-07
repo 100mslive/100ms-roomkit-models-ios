7f3b69005725fb09a19a985bfd714f71cda748d2
pawan-100ms <pawan@100ms.live>
2023-09-07 11:33:03 +0400

    Removed test target

--- HMSRoomModels/Package.swift
+++ HMSRoomModels/Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "HMSRoomModels",
             dependencies: []),
-        .testTarget(
-            name: "HMSRoomModelsTests",
-            dependencies: ["HMSRoomModels"]),
     ]
 )
