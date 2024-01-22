b8162b9f982d36fc01c225088284c3040e1438cb
pawan-100ms <pawan@100ms.live>
2024-01-22 12:57:03 +0400

    Added new control overlay and magnification gesture.

--- Sources/HMSRoomModels/HMSHLSPlayerView.swift
+++ Sources/HMSRoomModels/HMSHLSPlayerView.swift
@@ -68,6 +68,8 @@ public struct HMSHLSPlayerView<VideoOverlay> : View where VideoOverlay : View {
     var onPlaybackStateChanged: ((HMSHLSPlaybackState)->Void)?
     var onResolutionChanged: ((CGSize)->Void)?
     
+    @State private var scale: CGFloat = 1.0
+    @State private var lastScaleValue: CGFloat = 1.0
     
     @ViewBuilder let videoOverlay: ((HMSHLSPlayer) -> VideoOverlay)?
     public init(url: URL? = nil, @ViewBuilder videoOverlay: @escaping (HMSHLSPlayer) -> VideoOverlay) {
@@ -94,14 +96,37 @@ public struct HMSHLSPlayerView<VideoOverlay> : View where VideoOverlay : View {
     }
     
     func videoView(url: URL) -> some View {
-        VideoPlayer(player: coordinator.player._nativePlayer, videoOverlay: {
-            videoOverlay?(coordinator.player)
-        })
-        .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17)) {
-            $0.showsPlaybackControls = false
-            $0.allowsPictureInPicturePlayback = false
-            $0.canStartPictureInPictureAutomaticallyFromInline = false
-            AVPlayerModel.shared.currentAVPlayerInstance = $0
+        GeometryReader { geo in
+            
+            ScrollView([.vertical, .horizontal], showsIndicators: false) {
+                VideoPlayer(player: coordinator.player._nativePlayer)
+                    .introspect(.videoPlayer, on: .iOS(.v14, .v15, .v16, .v17)) {
+                        $0.showsPlaybackControls = false
+                        $0.allowsPictureInPicturePlayback = false
+                        $0.canStartPictureInPictureAutomaticallyFromInline = false
+                        AVPlayerModel.shared.currentAVPlayerInstance = $0
+                    }
+                    .frame(width: geo.size.width, height: geo.size.height )
+                    .scaleEffect(scale)
+                    .frame(width: geo.size.width * scale, height: geo.size.height * scale)
+                    .overlay(content: {
+                        Color.black.opacity(0.001)
+                            .gesture(MagnificationGesture().onChanged { val in
+                                let delta = val / self.lastScaleValue
+                                self.lastScaleValue = val
+                                var newScale = self.scale * delta
+                                if newScale < 1.0 {
+                                    newScale =  1.0
+                                }
+                                scale = newScale
+                            }.onEnded{val in
+                                lastScaleValue = 1
+                            })
+                    })
+                    .overlay(content: {
+                        videoOverlay?(coordinator.player)
+                    })
+            }
         }
         .onAppear() {
             coordinator.player.play(url)
