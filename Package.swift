63ba0b462dd7a728594c3b5b80081e7b26d47745
pawan-100ms <pawan@100ms.live>
2024-02-23 17:10:52 +0400

    Use the latest webrtc and HMSSDK with noise cancellation

--- Package.swift
+++ Package.swift
@@ -41,8 +41,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "HMSSDK",
-            url: "https://github.com/100mslive/100ms-ios-sdk-staging/releases/download/1.6.0/HMSSDK.xcframework.zip",
-                        checksum: "db0a7f9a15b27e12603db77b0a4f9e019fd7ea839c7441845124d8c6c2aca440"
+            url: "https://github.com/100mslive/100ms-ios-sdk-staging/releases/download/1.7.0/HMSSDK.xcframework.zip",
+                        checksum: "135155e1e7ab4525b646cb275ed87a71a91bf05bae39c56e83f5267adcf05e0c"
         ),
         .binaryTarget(
             name: "HMSAnalyticsSDK",
@@ -56,8 +56,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5116/WebRTC.xcframework.zip",
-            checksum: "5f38579bb743b089d95017fa56dc76f8e3e440dbdd56061db04c26448262cfee"
+            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5118/WebRTC.xcframework.zip",
+            checksum: "f80dde0250940647633340627f1a2aec83d8d53889e79ab34d0d168bcb16d253"
         ),
         .binaryTarget(
             name: "HMSBroadcastExtensionSDK",
