4d73adbd9f2f947c43ea94bdccd6b3ffee103fa7
pawan-100ms <pawan@100ms.live>
2024-02-29 12:14:04 +0400

    Using latest base sdk with stats fix

--- Package.swift
+++ Package.swift
@@ -41,8 +41,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "HMSSDK",
-            url: "https://github.com/100mslive/100ms-ios-sdk-staging/releases/download/1.7.0/HMSSDK.xcframework.zip",
-                        checksum: "135155e1e7ab4525b646cb275ed87a71a91bf05bae39c56e83f5267adcf05e0c"
+            url: "https://github.com/100mslive/100ms-ios-sdk-staging/releases/download/1.7.1/HMSSDK.xcframework.zip",
+                        checksum: "ec80ceec4be89fe0eb1efd49e55987adf83e90038971c18abed7581a155a175a"
         ),
         .binaryTarget(
             name: "HMSAnalyticsSDK",
