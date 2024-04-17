076f87f2113a4655d2a4248747b25f63c9095309
pawan-100ms <pawan@100ms.live>
2024-04-17 16:18:32 +0530

    Adding all binary as dependency by name

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HMSRoomModels",
-            dependencies: [.product(name: "JWTDecode", package: "JWTDecode.swift")]
+            dependencies: [.product(name: "JWTDecode", package: "JWTDecode.swift"), .byName(name: "HMSHLSPlayerSDK"), .byName(name: "HMSSDK"), .byName(name: "HMSAnalyticsSDK"), .byName(name: "WebRTC"), .byName(name: "HMSBroadcastExtensionSDK")]
         ),
         .binaryTarget(
             name: "HMSSDK",
