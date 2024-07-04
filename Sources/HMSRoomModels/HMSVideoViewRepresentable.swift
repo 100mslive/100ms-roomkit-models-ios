7df06d53ad0b2c907e78a799455649a61482b1bf
pawan-100ms <pawan@100ms.live>
2024-07-04 13:11:00 +0400

    Added: Support for unsubscribing video when track view is offscreen.

--- Sources/HMSRoomModels/HMSVideoViewRepresentable.swift
+++ Sources/HMSRoomModels/HMSVideoViewRepresentable.swift
@@ -12,15 +12,17 @@ public struct HMSVideoTrackView: View {
     
     @ObservedObject var peer: HMSPeerModel
     var contentMode: UIView.ContentMode
+    let unsubscribeWhenOffscreen: Bool
     
-    public init(peer: HMSPeerModel, contentMode: UIView.ContentMode = .scaleAspectFill) {
+    public init(peer: HMSPeerModel, contentMode: UIView.ContentMode = .scaleAspectFill, unsubscribeWhenOffscreen: Bool = false) {
         self.peer = peer
         self.contentMode = contentMode
+        self.unsubscribeWhenOffscreen = unsubscribeWhenOffscreen
     }
     
     public var body: some View {
         if let regularVideoTrackModel = peer.regularVideoTrackModel {
-            HMSTrackView(track: regularVideoTrackModel, contentMode: contentMode, isZoomAndPanEnabled: false)
+            HMSTrackView(track: regularVideoTrackModel, contentMode: contentMode, isZoomAndPanEnabled: false, unsubscribeWhenOffscreen: unsubscribeWhenOffscreen)
         }
     }
 }
@@ -30,16 +32,18 @@ public struct HMSScreenTrackView: View {
     @ObservedObject var peer: HMSPeerModel
     var contentMode: UIView.ContentMode
     var isZoomAndPanEnabled: Bool
+    let unsubscribeWhenOffscreen: Bool
     
-    public init(peer: HMSPeerModel, contentMode: UIView.ContentMode = .scaleAspectFit, isZoomAndPanEnabled: Bool = true) {
+    public init(peer: HMSPeerModel, contentMode: UIView.ContentMode = .scaleAspectFit, isZoomAndPanEnabled: Bool = true, unsubscribeWhenOffscreen: Bool = false) {
         self.peer = peer
         self.contentMode = contentMode
         self.isZoomAndPanEnabled = isZoomAndPanEnabled
+        self.unsubscribeWhenOffscreen = unsubscribeWhenOffscreen
     }
     
     public var body: some View {
         if let screenVideoTrackModel = peer.screenVideoTrackModel {
-            HMSTrackView(track: screenVideoTrackModel, contentMode: contentMode, isZoomAndPanEnabled: isZoomAndPanEnabled)
+            HMSTrackView(track: screenVideoTrackModel, contentMode: contentMode, isZoomAndPanEnabled: isZoomAndPanEnabled, unsubscribeWhenOffscreen: unsubscribeWhenOffscreen)
         }
     }
 }
@@ -49,29 +53,53 @@ public struct HMSTrackView: View {
     @ObservedObject var track: HMSTrackModel
     var contentMode: UIView.ContentMode
     var isZoomAndPanEnabled: Bool
+    let unsubscribeWhenOffscreen: Bool
     
-    public init(track: HMSTrackModel, contentMode: UIView.ContentMode, isZoomAndPanEnabled: Bool) {
+    @StateObject private var viewState = HMSVideoViewRepresentable.ViewState()
+    
+    public init(track: HMSTrackModel, contentMode: UIView.ContentMode, isZoomAndPanEnabled: Bool, unsubscribeWhenOffscreen: Bool = false) {
         self.track = track
         self.contentMode = contentMode
         self.isZoomAndPanEnabled = isZoomAndPanEnabled
+        self.unsubscribeWhenOffscreen = unsubscribeWhenOffscreen
     }
     
     public var body: some View {
         if let videoTrack = track.track as? HMSVideoTrack {
-            HMSVideoViewRepresentable(track: videoTrack, contentMode: contentMode, isZoomAndPanEnabled: isZoomAndPanEnabled)
+            
+            if unsubscribeWhenOffscreen {
+                HMSVideoViewRepresentable(track: videoTrack, contentMode: contentMode, isZoomAndPanEnabled: isZoomAndPanEnabled, viewState: viewState)
+                    .onAppear() {
+                        viewState.isOnScreen = true
+                    }
+                    .onDisappear() {
+                        viewState.isOnScreen = false
+                    }
+            }
+            else {
+                HMSVideoViewRepresentable(track: videoTrack, contentMode: contentMode, isZoomAndPanEnabled: isZoomAndPanEnabled, viewState: viewState)
+            }
         }
     }
 }
 
 internal struct HMSVideoViewRepresentable: UIViewRepresentable {
+    
+    internal class ViewState: ObservableObject {
+        @Published var isOnScreen: Bool = false
+    }
+    
     var track: HMSVideoTrack
     var contentMode: UIView.ContentMode
     var isZoomAndPanEnabled: Bool
     
-    init(track: HMSVideoTrack, contentMode: UIView.ContentMode = .scaleAspectFit, isZoomAndPanEnabled: Bool = false) {
+    @ObservedObject var viewState: ViewState
+    
+    init(track: HMSVideoTrack, contentMode: UIView.ContentMode = .scaleAspectFit, isZoomAndPanEnabled: Bool = false, viewState: ViewState) {
         self.track = track
         self.contentMode = contentMode
         self.isZoomAndPanEnabled = isZoomAndPanEnabled
+        self._viewState = ObservedObject(initialValue: viewState)
     }
 
     func makeUIView(context: Context) -> HMSVideoView {
@@ -83,9 +111,23 @@ internal struct HMSVideoViewRepresentable: UIViewRepresentable {
         return videoView
     }
 
-    func updateUIView(_ videoView: HMSVideoView, context: Context) {}
+    func updateUIView(_ videoView: HMSVideoView, context: Context) {
+        if viewState.isOnScreen {
+            onAppear(videoView, context: context)
+        } else {
+            onDisappear(videoView, context: context)
+        }
+    }
     
     static func dismantleUIView(_ uiView: HMSVideoView, coordinator: ()) {
         uiView.setVideoTrack(nil)
+    }
+    
+    func onAppear(_ videoView: HMSVideoView, context: Context) {
+        videoView.setVideoTrack(track)
+    }
+    
+    func onDisappear(_ videoView: HMSVideoView, context: Context) {
+        videoView.setVideoTrack(nil)
     }
 }
